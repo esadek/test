11dafa0cddb089c0be423e5328eceef923153fb7
Emil Sadek <esadek@hotmail.com>
2024-06-06 00:17:27 +0200

    Add: changes

--- .github/workflows/update_docs.py
+++ .github/workflows/update_docs.py
@@ -4,7 +4,7 @@ directory = os.path.dirname(__file__)
 
 print("In update_docs.py")
 
-with open(directory + "/../README.md") as f:
+with open(directory + "/../../README.md") as f:
     text = f.read()
 
 with open(directory + "/../DOCS.md", "w") as f:
