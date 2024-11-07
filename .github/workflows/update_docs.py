d9a965206186dc79aec9437e67215eccf319a462
Emil Sadek <esadek@users.noreply.github.com>
2024-11-07 22:19:38 +0100

    Remove: contents

--- .github/workflows/update_docs.py
+++
@@ -1,9 +0,0 @@
-import os
-
-directory = os.path.dirname(__file__)
-
-with open(directory + "/../../README.md") as f:
-    text = f.read()
-
-with open(directory + "/../../DOCS.md", "w") as f:
-    f.write(text)
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-<img src="resources/logo.png">
+# Test Repo
